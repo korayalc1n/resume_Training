8d1eb9fd4c103fdf73aeaf2832c058da06fec3ea
Koray <koraylcn6@gmail.com>
2025-05-04 13:56:16 +0300

    refactor: create urls in core app

--- resume/urls.py
+++ resume/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from xml.etree.ElementInclude import include
+
 from django.contrib import admin
-from django.urls import path
-from core.views import index
+from django.urls import path , include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
+    path('', include('core.urls')),
 ]
