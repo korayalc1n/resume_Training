127ad6e01a5d6c504ddc7bc8ba9a58de9b6140b7
Koray <koraylcn6@gmail.com>
2025-05-04 16:31:00 +0300

    feat: create static media tags and urls

--- resume/urls.py
+++ resume/urls.py
@@ -18,9 +18,17 @@ from xml.etree.ElementInclude import include
 
 from django.contrib import admin
 from django.urls import path , include
+from django.conf import settings
+from django.conf.urls.static import static
 
+from resume.settings import DEBUG, MEDIA_URL, STATIC_URL
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
